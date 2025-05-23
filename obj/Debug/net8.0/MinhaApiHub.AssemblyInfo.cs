2393b3972dc72b4597b46a11dc6488a09c689156
marciovenancio <marcio_vs@hotmail.com>
2025-05-23 17:28:36 -0300

    Alterações de teste na branch tst

--- obj/Debug/net8.0/MinhaApiHub.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinhaApiHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApiHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068275195895097e8fa6ccde96f1dd93d712adb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApiHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApiHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
